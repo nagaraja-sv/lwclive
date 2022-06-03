d33a556639dead9d8c99001f7581b998a15960ef
venkat <venkatanagarajas@gmail.com>
2022-06-03 09:41:07 +0530

    exploring on events

---
+++ unlimitedpower/force-app/main/default/lwc/exploreEventsChild/exploreEventsChild.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class ExploreEventsChild extends LightningElement {
+
+    handleClick(){
+        const recevieData = new CustomEvent('receivedata',{detail: 'Hey Salesforce Punk'});
+        this.dispatchEvent(recevieData);
+    }
+}
---
+++ unlimitedpower/force-app/main/default/lwc/exploreEventsParent/exploreEventsParent.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class ExploreEventsParent extends LightningElement {
+
+        message;
+
+    handleReceviedata(event){
+
+        this.message = event.detail;
+
+        console.log('event.detail'+ event.detail);
+
+    }
+}   
