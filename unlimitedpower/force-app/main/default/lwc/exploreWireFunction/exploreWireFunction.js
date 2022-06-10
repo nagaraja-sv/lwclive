dd5ed73e81d342a59d308fa120e55878d612e0c3
venkat <venkatanagarajas@gmail.com>
2022-06-10 08:04:57 +0530

    explore wire function

---
+++ unlimitedpower/force-app/main/default/lwc/exploreWireFunction/exploreWireFunction.js
@@ -0,0 +1,23 @@
+import { LightningElement, wire } from 'lwc';
+import getAccountsList from '@salesforce/apex/AccountController.getAccountsList';
+
+export default class ExploreWireFunction extends LightningElement {
+
+    accounts;
+    error;
+
+    @wire(getAccountsList)
+    wiredAccounts({error,data}){
+        if(data){
+            this.accounts = data;
+            this.error = undefined;
+
+        }else if(error){
+
+            this.error = error;
+            this.accounts = undefined;
+
+        }
+    }
+
+}   
